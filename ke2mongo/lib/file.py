4d90c689e67ca32fda297a1ae938aef62dcc1751
benscott <ben@benscott.co.uk>
2014-05-29 19:40:34 +0100

    Added file to lib

---
+++ ke2mongo/lib/file.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created by 'bens3' on 2013-06-21.
+Copyright (c) 2013 'bens3'. All rights reserved.
+"""
+
+import sys
+import os
+from ke2mongo import config
+
+def get_export_file_dates():
+    """
+    Gets all the dates of outstanding files
+    @return: list of dates
+    """
+
+    export_dir = config.get('keemu', 'export_dir')
+
+    files = [f for f in os.listdir(export_dir) if os.path.isfile(os.path.join(export_dir,f))]
+
+    # Use a set so we don't have duplicate dates
+    dates = set()
+
+    for f in files:
+
+        try:
+            # Extract the date from the file name
+            _, _, date, _ = f.split('.')
+        except ValueError:
+            # file not in the correct format - hidden directory etc.,
+            pass
+        else:
+            dates.add(int(date))
+
+    # Make sure they are in the right order
+    dates = sorted(dates)
+
+    return dates
