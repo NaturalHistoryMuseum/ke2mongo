41fa67df542715f2778e5f6fedee0490f312cb44
benscott <ben@benscott.co.uk>
2014-06-02 18:50:24 +0100

    Updated example

--- ke2mongo/run.py
+++ ke2mongo/run.py
@@ -4,7 +4,7 @@
 Created by 'bens3' on 2013-06-21.
 Copyright (c) 2013 'bens3'. All rights reserved.
 
-python run.py MongoTask --local-scheduler
+python run.py MainTask --local-scheduler --date 20140522
 
 """
 
