7a8dcee66f1373b1eac717fea21cd88d9a363f15
benscott <ben@benscott.co.uk>
2014-06-22 19:39:57 +0100

    Tidied up to allow calling alone in tests

--- ke2mongo/tasks/ke.py
+++ ke2mongo/tasks/ke.py
@@ -23,32 +23,42 @@ class KEFileTask(luigi.ExternalTask):
 
     module = luigi.Parameter()
     file_extension = luigi.Parameter()
+    export_dir = luigi.Parameter()
     date = luigi.IntParameter(default=None)
 
     def output(self):
-        return KEFileTarget(self.module, self.date, self.file_extension)
+        return KEFileTarget(self.export_dir, self.module, self.date, self.file_extension)
 
 
 class KEFileTarget(luigi.LocalTarget):
 
-    export_dir = config.get('keemu', 'export_dir')
     file_name = None
     is_tmp = False
 
-    def __init__(self, module, date, file_extension):
+    def __init__(self, export_dir, module, date, file_extension):
         """
         Override LocalTarget init to set path and format based on module and settings
         """
-        path, self.file_name, self.format = self.get_file(module, date, file_extension)
+
+        self.export_dir = export_dir
+        self.module = module
+        self.date = date
+        self.file_extension = file_extension
+
+        path, self.file_name, self.format = self.get_file()
         super(KEFileTarget, self).__init__(path, self.format)
 
-    def get_file(self, module, date, file_extension):
+    def get_file(self):
         """
         Loop through file and file.gz and return the one that exists
         If neither exists, raise an Exception
         """
 
-        file_name = '.'.join([module, file_extension, str(date)])
+        file_name_parts = [self.module, self.file_extension]
+        if self.date:
+            file_name_parts.append(str(self.date))
+
+        file_name = '.'.join(file_name_parts)
 
         # List of candidate files and types to try
         candidate_files = [
