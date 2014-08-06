d598e794b575e0867633b55ae373125405a78b6f
benscott <ben@benscott.co.uk>
2014-08-06 14:26:53 +0100

    Added eg

--- ke2mongo/tasks/mongo_catalogue.py
+++ ke2mongo/tasks/mongo_catalogue.py
@@ -4,7 +4,7 @@
 Created by 'bens3' on 2013-06-21.
 Copyright (c) 2013 'bens3'. All rights reserved.
 
-python run.py MongoCatalogueTask --local-scheduler --date 20140123
+python run.py MongoCatalogueTask --local-scheduler --date 20140605
 
 """
 
