bec3eb3fd3e64069cf822501820a0a998f849a14
benscott <ben@benscott.co.uk>
2014-08-11 15:08:07 +0000

    Added eg

--- ke2mongo/tasks/mongo_collection_index.py
+++ ke2mongo/tasks/mongo_collection_index.py
@@ -3,6 +3,9 @@
 """
 Created by 'bens3' on 2013-06-21.
 Copyright (c) 2013 'bens3'. All rights reserved.
+
+python run.py MongoCollectionIndexTask --local-scheduler --date 20140731
+
 """
 
 from ke2mongo.tasks.mongo import MongoTask
