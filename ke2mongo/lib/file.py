b21ed5e58e859aa8e5b823599eea287453224951
benscott <ben@benscott.co.uk>
2014-11-12 19:13:02 +0000

    Added bulk last export date

--- ke2mongo/lib/file.py
+++ ke2mongo/lib/file.py
@@ -7,6 +7,7 @@ Copyright (c) 2013 'bens3'. All rights reserved.
 
 import sys
 import os
+from ConfigParser import NoOptionError
 from ke2mongo import config
 
 def get_export_file_dates():
@@ -16,6 +17,12 @@ def get_export_file_dates():
     """
 
     export_dir = config.get('keemu', 'export_dir')
+
+    try:
+        full_export_date = int(config.get('keemu', 'full_export_date'))
+    except NoOptionError:
+        full_export_date = None
+
 
     files = [f for f in os.listdir(export_dir) if os.path.isfile(os.path.join(export_dir,f))]
 
@@ -34,7 +41,15 @@ def get_export_file_dates():
             # file not in the correct format - hidden directory etc.,
             pass
         else:
-            dates.add(int(date))
+
+            date = int(date)
+
+            # If we have full export date (the data the last full dump was produced)
+            # we only want dates after the last full dump - so skip prior dates
+            if full_export_date and date < full_export_date:
+                continue
+
+            dates.add(date)
 
     # Make sure they are in the right order and convert to list
     dates = sorted(list(dates))
