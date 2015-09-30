2c70f626b2746073e6f2850cc2890fcb008572b2
benscott <ben@benscott.co.uk>
2015-09-30 13:54:52 +0100

    tmp remove delete task

--- ke2mongo/tasks/specimen.py
+++ ke2mongo/tasks/specimen.py
@@ -5,7 +5,7 @@ Created by 'bens3' on 2013-06-21.
 Copyright (c) 2013 'bens3'. All rights reserved.
 
 python tasks/specimen.py SpecimenDatasetAPITask --local-scheduler
-python tasks/specimen.py SpecimenDatasetAPITask --local-scheduler --date 20150611
+python tasks/specimen.py SpecimenDatasetAPITask --local-scheduler --date 20150806
 
 NOTE: This started failing on my dev box, I think because indexes got corrupted
 Running rebuildIndexes() has fixed the problem
