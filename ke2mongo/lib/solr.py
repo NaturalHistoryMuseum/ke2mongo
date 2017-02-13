9e1264e77ea73418d543caa28fc59d0797a41101
benscott <ben@benscott.co.uk>
2017-02-13 18:42:58 +0000

    Adding back indexing

--- ke2mongo/lib/solr.py
+++ ke2mongo/lib/solr.py
@@ -36,8 +36,6 @@ class SolrIndex:
         }
         url = os.path.join(self.index, 'dataimport')
         r = requests.get(url, params=params)
-        print(r)
-        print(r.text)
         r.raise_for_status()
         return r.json()
 
@@ -57,21 +55,19 @@ def solr_reindex():
     for index in indexes:
         solr_index = SolrIndex(index)
         print("Starting full import of index: %s" % index)
-        r = solr_index.status()
-        print(r)
-        # solr_index.full_import()
+        solr_index.full_import()
 
-        # # Enter loop to keep checking status every SLEEP_INTERVAL
-        # while True:
-        #     r = solr_index.status()
-        #     if r['status'] == 'busy':
-        #         print('Total Rows Fetched: %s' % r['statusMessages'].get('Total Rows Fetched'))
-        #         print('Time elapsed: %s' % r['statusMessages'].get('Time Elapsed'))
-        #         time.sleep(SLEEP_INTERVAL)
-        #     else:
-        #         print(r['statusMessages'].get(''))
-        #         print('Time taken: %s' % r['statusMessages'].get('Time taken'))
-        #         break;
+        # Enter loop to keep checking status every SLEEP_INTERVAL
+        while True:
+            r = solr_index.status()
+            if r['status'] == 'busy':
+                print('Total Rows Fetched: %s' % r['statusMessages'].get('Total Rows Fetched'))
+                print('Time elapsed: %s' % r['statusMessages'].get('Time Elapsed'))
+                time.sleep(SLEEP_INTERVAL)
+            else:
+                print(r['statusMessages'].get(''))
+                print('Time taken: %s' % r['statusMessages'].get('Time taken'))
+                break;
 
 
 if __name__ == "__main__":
